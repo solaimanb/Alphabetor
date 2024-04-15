c6e0f13a3a535a169d086efd76025b3e712c4f52
soloman-err <soloman.err@gmail.com>
2024-04-16 02:32:08 +0600

    feat(db): database functionalities applied by integrating prisma, mongodb

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const allUsers = await prisma.user.findMany();
+  console.log(allUsers);
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
