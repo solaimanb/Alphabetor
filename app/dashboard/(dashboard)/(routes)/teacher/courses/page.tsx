e00e72879b7e43272acc76671931dc4365c542bd
soloman-err <soloman.err@gmail.com>
2024-04-16 00:33:58 +0600

    feat(course): course create functionality add

--- app/dashboard/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/dashboard/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const CoursesPage = () => {
   return (
     <div>
-      <Link href={"/teacher/create-course"} className="">
+      <Link href={"/dashboard/teacher/create"} className="">
         <Button>Add New Course</Button>
       </Link>
     </div>
