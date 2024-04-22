ea9d805274ed19f82055804da3bcf2e32c425b26
soloman-err <soloman.err@gmail.com>
2024-04-22 21:10:42 +0600

    feat(count-up): count up func add

--- next.config.mjs
+++ next.config.mjs
@@ -1,62 +1,24 @@
+/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "utfs.io",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.statically.io",
-      },
-      {
-        protocol: "https",
-        hostname: "cdn.jsdelivr.net",
-      },
-      {
-        protocol: "https",
-        hostname: "i.imgur.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "www.youtube.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.youtube.com",
-      },
-      {
-        protocol: "https",
-        hostname: "yt3.ggpht.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i1.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "yt3.ggpht",
-      },
-      {
-        protocol: "https",
-        hostname: "prod-discovery.edx-cdn.org",
-      },
+    domains: [
+      "utfs.io",
+      "images.unsplash.com",
+      "cdn.pixabay.com",
+      "cdn.statically.io",
+      "cdn.jsdelivr.net",
+      "i.imgur.com",
+      "i.ytimg.com",
+      "www.youtube.com",
+      "img.youtube.com",
+      "yt3.ggpht.com",
+      "i1.ytimg.com",
+      "i.ytimg.com",
+      "yt3.ggpht",
+      "prod-discovery.edx-cdn.org",
     ],
   },
 };
+
+export default nextConfig;
