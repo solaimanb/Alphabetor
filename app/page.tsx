63ba80d629e10cfd0f6e5ea07281f6e555b80611
soloman-err <soloman.err@gmail.com>
2024-04-23 12:08:13 +0600

    fix(issue): Hydration

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ const Home = () => {
     <>
       <Header />
       <Hero />
-      <Sponsors />
+      {/* <Sponsors /> */}
       <Counter />
       {/* <Testimonial /> */}
       <Newsletter />
